0bfecad9d0287fa6292dce275579dacb11a32f94
Nicholas Solovyev <nks@.lanl.gov>
2023-11-02 13:37:54 -0600

    upgraded init file for vulture upgrade

--- TELF/pre_processing/Vulture/__init__.py
+++ TELF/pre_processing/Vulture/__init__.py
@@ -1,7 +1,5 @@
 from .vulture import Vulture
 from .pre_process import *
-from .default_stop_words import default_stop_words
-from .default_stop_phrases import default_stop_phrases
-from .detect_language import get_language
-import sys
-sys.path += ["tokens_analysis/"]
+from .default_stop_words import STOP_WORDS
+from .default_stop_phrases import STOP_PHRASES
+
