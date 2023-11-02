5932092798b7756bc9407315246dbf2ede1a504d
Nicholas Solovyev <nks@.lanl.gov>
2023-11-02 13:26:06 -0600

    updated default stop phrases

--- TELF/pre_processing/Vulture/default_stop_phrases.py
+++ TELF/pre_processing/Vulture/default_stop_phrases.py
@@ -5,11 +5,9 @@ Created on Tue Jul 26 08:35:20 2022
 
 @author: maksim, nick
 """
-
-default_stop_phrases = [
+STOP_PHRASES = [
     'All rights reserved.',
     r'(?=Published by).*\.',  # remove statements with Published by
     r'(?=Approved for unlimited, public release).*\.',
-    r'(?=Prepared by LLNL under).*\.',
     r'(?=Copyright (19|20)\d{2} by).*\.',  # remove 'Copyright YEAR by' statements
 ]
