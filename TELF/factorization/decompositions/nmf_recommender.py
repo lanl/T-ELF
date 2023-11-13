41864baa4091fecd7eb5f9782c11f94d0bf1c6f7
MaksimEkin <meren1@umbc.edu>
2023-11-13 14:35:10 -0700

    Adding masking to RNMFk

--- TELF/factorization/decompositions/nmf_recommender.py
+++ TELF/factorization/decompositions/nmf_recommender.py
@@ -18,8 +18,9 @@ def nmf(X, W, H,
         lr_bu=.005,
         lr_bi=.005,
         global_mean=0,
-        calculate_global_mean=True
-        ):
+        calculate_global_mean=True,
+        KNOWN_MASK = None
+    ):
 
 
     np = get_np(X, use_gpu=use_gpu)
@@ -48,37 +49,43 @@ def nmf(X, W, H,
     n_users = X.shape[0]
     n_items = X.shape[1]
     
-    # nnz coords and entries
-    if use_gpu and HAS_CUPY and scipy.sparse.issparse(X):
-        rows, columns, entries = cupyx.scipy.sparse.find(X)
+    # known coords and entries
+    if KNOWN_MASK is None:
+        if use_gpu and HAS_CUPY and scipy.sparse.issparse(X):
+            rows, columns, entries = cupyx.scipy.sparse.find(X)
+        else:
+            rows, columns = X.nonzero()
+            if scipy.sparse.issparse(X):
+                entries = X.data
+            else:
+                entries = X[rows, columns]
     else:
-        rows, columns = X.nonzero()
+        indices = np.argwhere(KNOWN_MASK)
+        rows, columns = indices[:,0], indices[:,1]
+        rows, columns = np.array(rows), np.array(columns)
+
         if scipy.sparse.issparse(X):
-            entries = X.data
+            entries = np.array(X[rows, columns])[0]
         else:
             entries = X[rows, columns]
         
     # calculate the nnz users and ratings
-    if scipy.sparse.issparse(X):
-        if use_gpu:
-            user_counts = Counter(rows.get())
-            item_counts = Counter(columns.get())
-            n_ratings_users = []
-            n_ratings_items = []
-            
-            for idx in range(X.shape[0]):
-                n_ratings_users.append(user_counts[idx])
-            for idx in range(X.shape[1]):
-                n_ratings_items.append(item_counts[idx])
-            
-            n_ratings_users = np.array(n_ratings_users)
-            n_ratings_items = np.array(n_ratings_items)
-        else:
-            n_ratings_users = X.getnnz(axis=1)
-            n_ratings_items = X.getnnz(axis=0)
+    n_ratings_users = []
+    n_ratings_items = []
+    if use_gpu:
+        user_counts = Counter(rows.get())
+        item_counts = Counter(columns.get())
     else:
-        n_ratings_users = np.count_nonzero(X, axis=1)
-        n_ratings_items = np.count_nonzero(X, axis=0)
+        user_counts = Counter(rows)
+        item_counts = Counter(columns)
+            
+    for idx in range(X.shape[0]):
+        n_ratings_users.append(user_counts[idx])
+    for idx in range(X.shape[1]):
+        n_ratings_items.append(item_counts[idx])
+            
+    n_ratings_users = np.array(n_ratings_users)
+    n_ratings_items = np.array(n_ratings_items)
 
     # bias for users
     bu = np.zeros(n_users, dtype)
