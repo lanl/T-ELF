d559bf039980e2028970c5a9e41a2cdfc2f93b0d
Nicholas Solovyev <nks@.lanl.gov>
2023-11-15 07:39:05 -0700

    changed script to use updated Vulture

--- examples/Vulture/hpc/01-multi_node_parallel.py
+++ examples/Vulture/hpc/01-multi_node_parallel.py
@@ -1,37 +1,46 @@
 """
 Run this example with: sbatch 01-multi_node_parallel_job.sh
 """
-from TELF.pre_processing import Vulture
+import os
 import sys
 import pickle
+import pathlib
+
+from TELF.pre_processing import Vulture
 
 
-def main(argv):
+DATA_FILE = 'documents.p'
+DATA_DIR = os.path.join('..', '..', 'data')
+DATA_DIR = pathlib.Path(DATA_DIR).resolve()
 
-    # number of nodes and jobs within the node that will be used
-    n_nodes = int(argv[0])
-    n_jobs = int(argv[1])
+RESULTS_FILE = 'clean_documents.p'
+RESULTS_DIR = 'results'
+RESULTS_DIR = pathlib.Path(RESULTS_DIR).resolve()
+
+
+def main(n_jobs, n_nodes, verbose):
 
     #
-    # load data and stopwords
+    # load data
     #
-    documents = pickle.load(open("../../../data/documents.p", "rb"))
-    file = open("../../../data/stop_words.txt", "r")
-    stop_words = file.read().split("\n")
-    file.close()
+    documents = pickle.load(open(os.path.join(DATA_DIR, DATA_FILE), 'rb'))
 
     # clean
     settings = {
         "n_jobs": n_jobs,
         "n_nodes": n_nodes,
-        "verbose": 1,
-        "min_characters": 1,
-        "min_unique_characters": 2,
-        "lemmatize": True
+        "verbose": verbose,
     }
     vulture = Vulture(**settings)
-    vulture.clean(documents, stop_words, filename="clean_example")
+    vulture.clean(documents, save_path=os.path.join(RESULTS_DIR, RESULTS_FILE))
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+
+    parser = argparse.ArgumentParser(description='Vulture cleaning example script')
+    parser.add_argument('--n_jobs', type=int, help='Number of jobs to run', required=True)
+    parser.add_argument('--n_nodes', type=int, help='Number of nodes to use', required=True)
+    parser.add_argument('--verbose', type=int, help='Verbose mode: 0 for False, >=1 for True', default=1)
+    
+    args = parser.parse_args()
+    main(args.n_jobs, args.n_nodes, args.verbose)
