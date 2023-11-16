b57b23dc23bf5d7ab76d62718c48e88c5b33a6be
Nicholas Solovyev <nks@.lanl.gov>
2023-11-16 12:24:11 -0700

    bug fixes

--- examples/scripts/00-nmfk.py
+++ examples/scripts/00-nmfk.py
@@ -82,7 +82,13 @@ def init_vulture_steps(settings):
     return steps
     
     
-def main(file_path, cols, n_jobs, n_nodes, verbose):
+def main(file_path, cols, n_nodes, n_jobs, verbose):
+    
+    print(f'{file_path=}')
+    print(f'{cols=}')
+    print(f'{n_jobs=}')
+    print(f'{n_nodes=}')
+    
     config = load_config(CONFIG_PATH)
     
     # get settings from config 
@@ -90,17 +96,17 @@ def main(file_path, cols, n_jobs, n_nodes, verbose):
     
     vulture_steps = init_vulture_steps(config['vulture']['steps'])
     
-    nmfk_settings = config['nmfk']['decomposition_settings']
+    nmfk_settings = config['nmfk']['settings']
     nmfk_ks = range(config['nmfk']['ks']['start'], config['nmfk']['ks']['stop'], config['nmfk']['ks']['step'])
     
     beaver_settings = config['beaver']['settings']
-    beaver_vocabulary config['beaver']['vocabulary']
+    beaver_vocabulary = config['beaver']['vocabulary']
     if beaver_vocabulary is not None:
         beaver_vocabulary = load_list(beaver_vocabulary)
         
     # setup output directory
     file_path = pathlib.Path(file_path)
-    file_name = pathlib.Path.stem
+    file_name = file_path.stem
     output_dir = os.path.join(output_dir, file_name)
     output_dir = check_path(output_dir)
     
@@ -109,7 +115,7 @@ def main(file_path, cols, n_jobs, n_nodes, verbose):
     beaver_dir = check_path(os.path.join(output_dir, 'beaver'))
     
     # load the input DataFrame
-    df = pd.read_csv(os.path.join(input_path, fn))
+    df = pd.read_csv(file_path)
     if not set(cols).issubset(df.columns):
         raise ValueError('One or more specified columns was not found in the DataFrame!')
     
@@ -122,6 +128,7 @@ def main(file_path, cols, n_jobs, n_nodes, verbose):
                                  append_to_original_df=True, 
                                  concat_cleaned_cols=True,
                                 )
+    df.to_csv(os.path.join(beaver_dir, f'clean_{file_name}.csv'), index=False)
         
     # create Beaver object to generate matrices
     beaver = Beaver()
@@ -166,7 +173,7 @@ def main(file_path, cols, n_jobs, n_nodes, verbose):
     print(f'[tELF Script]: X={X_shape}')
 
     # decompose X
-    dynamic_settings, all_params = find_param_combinations(decomposition_settings)
+    dynamic_settings, all_params = find_param_combinations(nmfk_settings)
     for params in tqdm(all_params, total=len(all_params)):
 
         # update decomposition params for this instance
@@ -195,7 +202,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Full example of TELF library. A CSV file is cleaned with Vulture to create a " \
                                                  "Documents by Words TF-IDF matrix which is then decomposed by NMFk")
     parser.add_argument("-p", "--path", type=str, required=True, help="The path to the input CSV file")
-    parser.add_argument("-c", "--cols", nargs='+', required=True, help="The columns of the DataFrame which to clean and utilize)
+    parser.add_argument("-c", "--cols", nargs='+', required=True, help="The columns of the DataFrame which to clean and utilize")
     parser.add_argument("-n", "--n_nodes", type=int, required=True, help="Number of nodes (integer > 0)")
     parser.add_argument("-j", "--n_jobs", type=int, required=True, help="Number of jobs (integer > 0)")
     args = parser.parse_args()
