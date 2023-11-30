7f079c1f2680d3a9761bb50b1a07de8f457fc1d9
Nicholas Solovyev <nks@.lanl.gov>
2023-11-30 11:39:39 -0700

    added repr

--- TELF/pre_processing/Vulture/modules/module_base.py
+++ TELF/pre_processing/Vulture/modules/module_base.py
@@ -1,24 +1,32 @@
 class VultureModuleBase:
     
-    def __init__(self, frozen=None):
+    def __init__(self, frozen=None, max_display=25):
         # store the initialization parameters
-        self.init_args = (frozen,)
-        self.init_kwargs = {}
         self.frozen = frozen
+        self.max_display = max_display
+        
+    def __repr__(self):
+        classname = self.__class__.__name__
+        attributes = ', '.join(f"{key}={self._format_attr(value)}" for key, value in self.__dict__.items()
+                              if key not in {'max_display'} and not key.startswith('_'))
+        return f"{classname}({attributes})"
 
+    def _format_attr(self, value):
+        if isinstance(value, list):
+            return self._format_list(value)
+        return repr(value)
+
+    def _format_list(self, lst):
+        if len(lst) > self.max_display:
+            return f"[{', '.join(repr(x) for x in lst[:self.max_display])}, ... (+{len(lst) - self.max_display} more)]"
+        return repr(lst)
+    
     def _should_preserve(self, token):
         """Check if the token should be preserved and not cleaned."""
         return token in self.frozen
     
     def run(self, text):
         raise NotImplementedError("Subclasses must implement this method!")
-        
-    def get_init_params(self):
-        """Retrieve initialization parameters."""
-        return {
-            'args': self.init_args, 
-            'kwargs': self.init_kwargs
-        }
 
     
     # GETTERS / SETTERS
