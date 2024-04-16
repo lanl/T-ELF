5caa365a7c47b977b70bdb5b6704a21557fe34be
Ryan Barron <barron@lanl.gov>
2024-04-16 15:13:00 -0600

    closes issues #103, and #124. TODO -- #104

--- TELF/pre_processing/Vulture/default_stop_phrases.py
+++ TELF/pre_processing/Vulture/default_stop_phrases.py
@@ -7,6 +7,8 @@ Created on Tue Jul 26 08:35:20 2022
 """
 STOP_PHRASES = [
     'All rights reserved.',
+    'which stands for',
+    'standing for',
     r'(?=Published by).*\.',  # remove statements with Published by
     r'(?=Approved for unlimited, public release).*\.',
     r'(?=Copyright (19|20)\d{2} by).*\.',  # remove 'Copyright YEAR by' statements
